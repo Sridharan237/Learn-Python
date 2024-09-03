6653159933fb8383ffa7b4dc96f24bc00c14c43a
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-09-03 22:21:03 +0530

    Added variablenamingrules.py to Learn Python->2.Python Datatypes->Rules for declaring variable names

---
+++ 2.Python Datatypes/Rules for declaring variable names/variablenamingrules.py
@@ -0,0 +1,38 @@
+# rules for naming identifiers
+a = 1
+print(a)
+
+item1 = 10
+print(item1)
+
+x1 = 10
+# 1x = 10
+    ^
+
+# name of item = 'Mithun'   # syntax error - space is also a special character
+  
+
+item-price = 15
+  
+# item$price = 15     # syntax error - variable name must not contain special character except _ (underscore)
+
+item_price = 15
+print(item_price)
+
+x1 = 10
+# 1x = 10     # syntax error - variable name must not start with a number
+
+_x = 10
+x________1 = 20
+print(x_______1)
+
+print(x________1)
+
+a = 10
+A = 10
+A = 'string'
+
+type(a)
+type(A)
+
+print("a :", "A:", A)
