b840279171a95ad2f3b80bd4658b501199bd667d
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-09-04 21:55:08 +0530

    Added 2.Python Datatypes->Literals or Constants->basic literals->Numeric Literals->numericliterals.py

---
+++ 2.Python Datatypes/Literals or Constants/basic literals/Numeric Literals/numericliterals.py
@@ -0,0 +1,50 @@
+a = 1
+print(a)
+
+a = 12345
+print(a)
+
+a = 12_345 # we give _(underscore) to easily understand the number system
+print(a)
+
+a = 13.52
+print(a)
+
+a = 1352E-2
+print(a)
+
+a = 13_52E-2
+print(a)
+
+a = 1352_839.11_98_12
+print(a)
+
+# a = 13_._52
+#   File "<stdin>", line 1
+#     a = 13_._52
+#           ^
+# SyntaxError: invalid decimal literal
+
+a = True
+print(a)
+
+a = False
+print(a)
+
+a = 5 + 4j
+print(a)
+
+a = 5_4 + 4_4j
+print(a)
+
+name = 'john'
+print(name)
+
+name = "john"
+print(name)
+
+name = "'john'"
+print(name)
+
+name = '"john"'
+print(name)
