a87df8a92b91fe9bd346f7d27267bf7d2f028e25
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-09-04 18:34:03 +0530

    Added 2.Python Datatypes->numeric datatypes->intfloat.py

---
+++ 2.Python Datatypes/numeric datatypes/intfloat.py
@@ -0,0 +1,28 @@
+# int - datatype
+a = 1
+a = 123333333333333333333333333333333333232423423423
+print(a)
+
+print(type(a))
+print(a.__sizeof__())
+
+print(a = 1)
+
+
+# float - datatype
+x = 12.25
+print(x)
+
+x = 1225E-2
+print(x)
+
+x = 1255e-2
+print(x)
+
+x = 12.25e-2
+print(x)
+
+x = 123456789.123456789
+print(x)
+
+print(x.__sizeof__())
