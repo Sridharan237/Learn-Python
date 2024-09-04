87b7043aa8d6bc09e8e7ae46c6d31e23d700150b
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-09-04 19:42:20 +0530

    Added 2.Python Datatypes->numeric datatypes->boolcomplex.py

---
+++ 2.Python Datatypes/numeric datatypes/boolcomplex.py
@@ -0,0 +1,27 @@
+a = True
+print(a)
+
+print(int(a))
+
+b = 1
+print(bool(b))
+
+c = 17
+print(bool(c))
+
+c = 0
+print(bool(c))
+
+x = 5 + 10j
+print(x)
+
+print(x.real)
+
+print(x.imag)
+
+y = complex(10, 20)
+print(y)
+
+print(y.real)
+
+print(y.imag)
