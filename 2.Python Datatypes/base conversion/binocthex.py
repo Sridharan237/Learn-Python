06a7a2912c76a5af51946194524f32dea137ead4
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-09-05 18:45:30 +0530

    Added 2.Python Datatypes/base conversion/binocthex.py

---
+++ 2.Python Datatypes/base conversion/binocthex.py
@@ -0,0 +1,44 @@
+a = 10
+b = 16
+c = 255
+
+# integer literals
+print(bin(a))
+
+print(bin(b))
+
+print(bin(c))
+
+print(oct(a))
+
+print(oct(b))
+
+print(oct(c))
+
+print(hex(a))
+
+print(hex(b))
+
+print(hex(15))
+
+# boolean literals
+print(bin(True))
+
+print(oct(True))
+
+print(hex(True))
+
+
+# float literals
+# print(bin(12.5))
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: 'float' object cannot be interpreted as an integer
+
+
+# complex literals
+# print(bin(5 + 6j))
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: 'complex' object cannot be interpreted as an integer
+
