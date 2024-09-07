c18bf02664b7789e417d7551b4c27dbb4ba61650
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-09-07 13:19:32 +0530

    Added (KmtoMiles.py, CircleArea.py) to 3.Operators and Expressions/Student Challenge

---
+++ 3.Operators and Expressions/Student Challenge/CircleArea.py
@@ -0,0 +1,9 @@
+# python program to find the area of a circle
+
+import math
+
+radius = int(input('Enter radius of the circle : '))
+
+area = math.pi * radius * radius
+
+print('Area :', area)
---
+++ 3.Operators and Expressions/Student Challenge/KmToMiles.py
@@ -0,0 +1,7 @@
+# python program to convert kilometers to miles
+
+km = int(input('Enter the distance (in kms) : '))
+
+miles = km * 0.621371
+
+print("Distance (in miles) :", miles)
