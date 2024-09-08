3ed6a7c887ffbc5943f06479a6cd753dd5cc61f6
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-09-08 06:21:43 +0530

    Added 4.Conditional Statements/Checking whether Positive, Negative, Zero/PosNegativeZero.py

---
+++ 4.Conditional Statements/Checking whether Positive, Negative, Zero/PosNegativeZero.py
@@ -0,0 +1,10 @@
+# python program to check whether a number is positive (or) Negative (or) Zero
+
+n = int(input('Enter a number : '))
+
+if n > 0:
+    print('Positive')
+elif n < 0:
+    print('Negative')
+else:
+    print('Zero')
