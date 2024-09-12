8dadba62cdf93ce1afe46b520a10bdcea04f097f
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-09-12 05:34:54 +0530

    Added 6.Loops/ForLoop/ForLoopDemo.py

---
+++ 6.Loops/ForLoop/ForLoopDemo.py
@@ -0,0 +1,23 @@
+# python program to demonstrate for loop in python
+
+# method1 : using a sequence(collection)
+# message = 'Hello'
+#
+# for x in message:
+#     print(x)
+
+# method2 : using range() function
+for i in range(10):
+    print(i)
+
+for i in range(0, 10):
+    print(i)
+
+for i in range(0, 10, 2):
+    print(i)
+
+for i in range(10, 0, -1):
+    print(i)
+
+for i in range(10, 0, -2):
+    print(i)
