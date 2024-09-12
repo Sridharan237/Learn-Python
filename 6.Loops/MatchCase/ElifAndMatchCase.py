341b4e015f65a15359e68c207116b774f7c93938
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-09-12 21:48:52 +0530

    Added 6.Loops/MatchCase/ElifAndMatchCase.py

---
+++ 6.Loops/MatchCase/ElifAndMatchCase.py
@@ -0,0 +1,42 @@
+# python program to implement match case for printing days of a week using day number
+
+day = int(input('Enter day number : '))
+
+# method-1 -> using elif statements
+
+# if day == 1:
+#     print('Sunday')
+# elif day == 2:
+#     print('Monday')
+# elif day == 3:
+#     print('Tuesday')
+# elif day == 4:
+#     print('Wednesday')
+# elif day == 5:
+#     print('Thursday')
+# elif day == 6:
+#     print('Friday')
+# elif day == 7:
+#     print('Saturday')
+# else:
+#     print('Invalid day number!')
+
+# method-2 -> using match case
+
+match day:
+    case 1:
+        print('Sunday')
+    case 2:
+        print('Monday')
+    case 3:
+        print('Tuesday')
+    case 4:
+        print('Wednesday')
+    case 5:
+        print('Thursday')
+    case 6:
+        print('Friday')
+    case 7:
+        print('Saturday')
+    case _:
+        print('Invalid day number!')
