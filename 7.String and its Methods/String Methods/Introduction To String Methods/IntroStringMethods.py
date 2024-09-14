c92680242232f290e8567beff44cd4f5947ce0e7
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-09-14 17:31:45 +0530

    Added 7.String and its Methods\String Methods\Introduction To String Methods\IntroStringMethods.py

---
+++ 7.String and its Methods/String Methods/Introduction To String Methods/IntroStringMethods.py
@@ -0,0 +1,33 @@
+# python program to implement the dir(), help() functions
+
+s = 'hello hai'
+
+dir(s)
+
+# Output : ['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mod__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmod__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'capitalize', 'casefold', 'center', 'count', 'encode', 'endswith', 'expandtabs', 'find', 'format', 'format_map', 'index', 'isalnum', 'isalpha', 'isascii', 'isdecimal', 'isdigit', 'isidentifier', 'islower', 'isnumeric', 'isprintable', 'isspace', 'istitle', 'isupper', 'join', 'ljust', 'lower', 'lstrip', 'maketrans', 'partition', 'removeprefix', 'removesuffix', 'replace', 'rfind', 'rindex', 'rjust', 'rpartition', 'rsplit', 'rstrip', 'split', 'splitlines', 'startswith', 'strip', 'swapcase', 'title', 'translate', 'upper', 'zfill']
+
+help(s.split)
+
+# Output :-
+'''
+Help on built-in function split:
+
+split(sep=None, maxsplit=-1) method of builtins.str instance
+    Return a list of the substrings in the string, using sep as the separator string.
+
+      sep
+        The separator used to split the string.
+
+        When set to None (the default value), will split on any whitespace
+        character (including \n \r \t \f and spaces) and will discard
+        empty strings from the result.
+      maxsplit
+        Maximum number of splits.
+        -1 (the default value) means no limit.
+
+    Splitting starts at the front of the string and works to the end.
+
+    Note, str.split() is mainly useful for data that has been intentionally
+    delimited.  With natural text that includes punctuation, consider using
+    the regular expression module.
+'''
