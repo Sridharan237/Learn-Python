d82895a7185df9dcc38f12075cd39e2169f7d04f
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-09-15 16:03:28 +0530

    Added 7.String and its Methods\String Methods\StringMethods2.py

---
+++ 7.String and its Methods/String Methods/StringMethods2.py
@@ -0,0 +1,29 @@
+# python program to implement the string functions : ljust, rjust, center, lstrip, rstrip, strip
+
+s = 'python'
+
+print(s.ljust(10))  # Output : python
+print(s.ljust(10)) # Output : 'python    '
+print(s.rjust(10)) # Output : '    python'
+s.center(len(s)+1, '*') # Output : '*python'
+print(s.center(10, '*'))   # Output : '**python**'
+print(len(s))   # Output : 6
+print(s.ljust(len(s))) # Output : 'python'
+print(s.ljust(len(s)+1))   # Output : 'python '
+
+s1 = '   ... ... +++ aaa python'
+
+print(s1.lstrip()) # Output : '... ... +++ aaa python'
+print(s1)  # Output : '   ... ... +++ aaa python'
+print(s1.lstrip('.'))  # Output : '   ... ... +++ aaa python'
+s2 = s1.lstrip('.')    # s2 - '   ... ... +++ aaa python'
+s2 = s1.lstrip()       # s2 - '... ... +++ aaa python'
+print(s2)  # Output : '... ... +++ aaa python'
+print(s2.lstrip('.'))  # Output : ' ... +++ aaa python'
+print(s2.lstrip('.+')) # Output : ' ... +++ aaa python'
+print(s2.lstrip()) # Output : '... ... +++ aaa python'
+print(s2.lstrip('.+')) # Output : ' ... +++ aaa python'
+s2.lstrip('.+ ')    # Output : 'aaa python'
+s3 = '... +++ aaa python aaa ... +++'
+print(s3.rstrip('+'))  # Output : '... +++ aaa python aaa ... '
+print(s3.strip('.+a '))    # Output : 'python'
