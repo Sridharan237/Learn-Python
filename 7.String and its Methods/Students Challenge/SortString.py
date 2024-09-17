6261161e8c56d458cd47cb4c10025e280a9caba7
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-09-17 13:02:57 +0530

    Added 7.String and its Methods/Students Challenge/SortString.py

---
+++ 7.String and its Methods/Students Challenge/SortString.py
@@ -0,0 +1,12 @@
+# python program to sort all the characters of a string
+
+s = 'adkbeiekpqlmna'
+
+result_list = sorted(s)
+
+print(result_list)
+
+sorted_string = ''.join(result_list)    # converting a list of strings to a single string
+
+print(sorted_string)
+
