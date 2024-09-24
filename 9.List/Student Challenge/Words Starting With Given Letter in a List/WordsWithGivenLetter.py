3e6fc56b073195fcabcc8619857dd633c9bd91f8
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-09-24 22:45:40 +0530

    Added 9.List/Student Challenge/Words Starting With Given Letter in a List/WordsWithGivenLetter.py

---
+++ 9.List/Student Challenge/Words Starting With Given Letter in a List/WordsWithGivenLetter.py
@@ -0,0 +1,17 @@
+# python program to find the words starting with a given letter in a list
+
+food = ['pizza', 'burger', 'nuggets', 'pasta', 'noodles', 'parotta']
+
+letter = input('Enter a small case alphabet : ')    # letter - small case alphabet character
+
+print('Words Starting With Given Letter are :')
+
+flag = 0
+
+for item in food:
+    if letter == item[0]:
+        print(item)
+        flag = 1
+else:
+    if flag == 0:
+        print('No Words Starting With Given Letter Found')
