2e6f70f67ae2b4b78fcfd5ddbf390cab691aeada
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-10-09 22:48:21 +0530

    Added some changes to 13.Functions/Local vs Global Variable/LocalvsGlobal.py

--- 13.Functions/Local vs Global Variable/LocalvsGlobal.py
+++ 13.Functions/Local vs Global Variable/LocalvsGlobal.py
@@ -55,3 +55,24 @@ print('Global outside function fun4 \'g\' :', g)
 fun4(10, 20)
 
 print('Global outside function fun4 \'g\' :', g)
+
+
+
+# built-in functions => locals(), globals()
+
+def fun5(a, b):
+    c = a + b
+    print(c)
+    print(locals())
+    # Output : {'a': 4, 'b': 8, 'c': 12}
+
+    print(globals())
+    # Output : {'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <_frozen_importlib_external.SourceFileLoader object at 0x000001F11565BDA0>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': 'D:\\Learn-Github-Repositories\\Learn Python Programming - Beginner to Master\\13.Functions\\Local vs Global Variable\\LocalvsGlobal.py', '__cached__': None, 'g': 15, 'fun1': <function fun1 at 0x000001F1158E8EA0>, 'fun2': <function fun2 at 0x000001F1158E8CC0>, 'fun4': <function fun4 at 0x000001F1158E8F40>, 'fun5': <function fun5 at 0x000001F1158E8FE0>}
+
+fun5(4, 8)
+
+print(locals())
+# Output : {'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <_frozen_importlib_external.SourceFileLoader object at 0x000001F11565BDA0>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': 'D:\\Learn-Github-Repositories\\Learn Python Programming - Beginner to Master\\13.Functions\\Local vs Global Variable\\LocalvsGlobal.py', '__cached__': None, 'g': 15, 'fun1': <function fun1 at 0x000001F1158E8EA0>, 'fun2': <function fun2 at 0x000001F1158E8CC0>, 'fun4': <function fun4 at 0x000001F1158E8F40>, 'fun5': <function fun5 at 0x000001F1158E8FE0>}
+
+print(globals())
+# Output : {'__name__': '__main__', '__doc__': None, '__package__': None, '__loader__': <_frozen_importlib_external.SourceFileLoader object at 0x000001F11565BDA0>, '__spec__': None, '__annotations__': {}, '__builtins__': <module 'builtins' (built-in)>, '__file__': 'D:\\Learn-Github-Repositories\\Learn Python Programming - Beginner to Master\\13.Functions\\Local vs Global Variable\\LocalvsGlobal.py', '__cached__': None, 'g': 15, 'fun1': <function fun1 at 0x000001F1158E8EA0>, 'fun2': <function fun2 at 0x000001F1158E8CC0>, 'fun4': <function fun4 at 0x000001F1158E8F40>, 'fun5': <function fun5 at 0x000001F1158E8FE0>}
