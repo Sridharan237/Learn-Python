1206866f0754e2dc08b626941e65fdbe1822c054
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-10-19 15:37:56 +0530

    Added some changes to 15.Exception Handling\Why try-except\WhyTryExcept.py

--- 15.Exception Handling/Why try-except/WhyTryExcept.py
+++ 15.Exception Handling/Why try-except/WhyTryExcept.py
@@ -1,41 +1,42 @@
 # python program to implement exception handling using try an except blocks and why we require try and except blocks
 
-# # Example 1 => program handles ZeroDivisionError Exception - without using try-except blocks 
+# Example 1 => program handles ZeroDivisionError Exception - without using try-except blocks 
 
-# a = int(input('Enter first number : '))
-# b = int(input('Enter second number : '))
+a = int(input('Enter first number : '))
+b = int(input('Enter second number : '))
 
-# if b != 0:
-#     c = a // b
-#     print('Division :', c)
+if b != 0:
+    c = a // b
+    print('Division :', c)
     
-# else:
-#     print('Zero Division Error')
+else:
+    print('Zero Division Error')
     
 
 
-# # Example 2 => program handles ZeroDivisionError Exception - without using try-except blocks and divides using custom function div()
+# Example 2 => program handles ZeroDivisionError Exception - without using try-except blocks and divides using custom function div()
 
-# # function to take 2 arguments and returns quotient (or) return -1 if second number is 0
-# def div(a, b):
-#     if b != 0:
-#         c = a // b
-#         return c
-#     else:
-#         return -1   # -1 - means ZeroDivisionError  => Division by zero
+# function to take 2 arguments and returns quotient (or) return -1 if second number is 0
+def div(a, b):
+    if b != 0:
+        c = a // b
+        return c
+    else:
+        return -1   # -1 - means ZeroDivisionError  => Division by zero
 
 
-# a = int(input('Enter first number : '))
-# b = int(input('Enter second number : '))
+a = int(input('Enter first number : '))
+b = int(input('Enter second number : '))
 
-# c = div(a, b)
+c = div(a, b)
 
-# if c == -1:
-#     print('Zero Division Error')
+if c == -1:
+    print('Zero Division Error')
     
-# else:
-#     print('Division :', c)
+else:
+    print('Division :', c)
     
+
 
 # Example 3 => program handles ZeroDivisionError Exception - with using try-except blocks and divides using custom function div() 
 
