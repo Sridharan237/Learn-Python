aaa81e903163fbb172048f8afc5b845e28668ee4
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-10-23 17:43:02 +0530

    Added 17.Object Oriented Programming\Instance Variables and Instance Methods\Instance variables and Instance Methods.py

---
+++ 17.Object Oriented Programming/Instance Variables and Instance Methods/Instance variables and Instance Methods.py
@@ -0,0 +1,45 @@
+# python program to implement Instance Variables and Instance Methods
+
+# Example 1 => class for Rectangle
+# length, breadth - instance variables of class Rectangle
+# area, perimeter - instance methods of class Rectangle
+class Rectangle:
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+    
+    def area(self):
+        return self.length * self.breadth
+    
+    def perimeter(self):
+        return 2 * (self.length + self.breadth)
+    
+r1 = Rectangle(10, 5)
+
+r2 = Rectangle(20, 15)
+
+
+
+# Example 2 => class for Test
+# a, b, c  - instance variables of class Test
+# fun - instance methods of class Test
+class Test:
+    def __init__(x):
+        x.a = 10
+    
+    def fun(y):
+        y.b = 20
+        
+t1 = Test()
+
+t1.c = 30
+
+print(dir(t1))
+
+# Output : ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'a', 'c', 'fun']
+
+t1.fun()
+
+print(dir(t1))
+
+# Output : ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'a', 'b', 'c', 'fun']
