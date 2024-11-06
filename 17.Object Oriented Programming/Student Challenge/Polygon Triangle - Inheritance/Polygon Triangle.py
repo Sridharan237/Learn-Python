7a8bf97afb1d5acd07ed0705729067f08cd287c4
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-11-06 16:34:22 +0530

    Added 17.Object Oriented Programming\Student Challenge\Polygon Triangle - Inheritance\Polygon Triangle.py

---
+++ 17.Object Oriented Programming/Student Challenge/Polygon Triangle - Inheritance/Polygon Triangle.py
@@ -0,0 +1,31 @@
+# python program to implement a polygon class and triangle class
+
+import math
+
+# class for polygon
+class Polygon:
+    
+    def __init__(self, no_of_sides, sides):
+        self.no_of_sides = no_of_sides
+        self.sides = sides
+    
+# class for triangle which inherits from polygon
+class Triangle(Polygon):
+    
+    def __init__(self, no_of_sides, sides):
+        super().__init__(no_of_sides, sides)
+    
+    def area(self):
+        # unpacking from list 'sides'
+        a, b, c = self.sides
+        
+        s = (a + b + c) / 2
+        
+        area = math.sqrt(s * (s-a) * (s-b) * (s-c))
+
+        return area
+
+# creating object for Triangle class
+t = Triangle(3, [40, 50, 30])
+
+print('Area :', t.area())
