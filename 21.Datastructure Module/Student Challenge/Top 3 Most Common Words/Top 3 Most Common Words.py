1ec6713948085df927620b56030d89b42c69ffe6
Sridharan237 <sridharan.a2022cse@sece.ac.in>
2024-11-13 18:16:20 +0530

    Added some changes to 21.Datastructure Module\Student Challenge\Top 3 Most Common Words\Top 3 Most Common Words.py

--- 21.Datastructure Module/Student Challenge/Top 3 Most Common Words/Top 3 Most Common Words.py
+++ 21.Datastructure Module/Student Challenge/Top 3 Most Common Words/Top 3 Most Common Words.py
@@ -1,17 +1,16 @@
 # python program to find top 3 most common words in a multiline string literal
 
 from collections import Counter
+import re
 
-text = '''Python is an easy programmers language.
-Python syntax is like the English language.
-Python language is easy to learn and adapt to compared with Java and C.
-In Python language, the same task can be performed using fewer lines of code.
-its easy learning and easy to code.'''
+f = open('D://Learn-Github-Repositories//Learn Python Programming - Beginner to Master//21.Datastructure Module//Student Challenge//text.txt', 'r')
 
-L = text.replace('.', '').split()
+text = f.read()
 
-# creating Counter object from sequence L
-c = Counter(L)
+words = re.findall('\w+', text)
+
+# creating Counter object from sequence 'words'
+c = Counter(words)
 
 print(c.most_common(3))
-# Output : [('Python', 4), ('easy', 4), ('is', 3)]
+# Output : [('Python', 4), ('easy', 4), ('language', 4)]
